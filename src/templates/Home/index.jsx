d1192a87342ddfb614cd903da0a3bab4ee6e0130
valdineia-martins <valdineia.dev@gmail..com>
2023-07-07 22:32:28 -0300

    Trabalhando com inputs de texto

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,14 +1,20 @@
 import { Component } from 'react';
+
 import './styles.css'
 
 import { Posts } from '../../components/Posts'; 
-
 import {loadPosts} from '../../utils/load-posts';
+import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 
 export class Home extends Component {
   state = {    
-    posts: []
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 2,
+    searchValue: ''
   };
 
 
@@ -17,27 +23,81 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+   const { page, postsPerPage } =this.state;
+
    const postsAndPhotos = await loadPosts();
-   this.setState({ posts: postsAndPhotos });
-   
+   this.setState({
+     posts: postsAndPhotos.slice(page, postsPerPage),
+     allPosts: postsAndPhotos,
+    });
+   }
+
+   loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts,
+    } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
     
-  }
+    
+   }
+
+   handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({searchValue: value});
+   }
 
 
   render() {
-    const {posts} = this.state;
-    
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ? 
+    allPosts.filter( post => {
+      return post.title.toLowerCase().includes(
+        searchValue.toLocaleLowerCase()
+        );
+    })
+    : posts;
 
     return (
-    
-      <section className='container'>
-        <Posts posts={posts} />
-      </section>
-     
-    );
-  }
+     <section className="container">
 
- 
+        <div className="search-container">
+             {!!searchValue && (
+             <h1>Search value: {searchValue}</h1>  
+             )}
+
+            <TextInput searchValue={searchValue} handleChange={this.handleChange}/>
+        </div>
+
+         {filteredPosts.length > 0 && (
+            <Posts posts={filteredPosts} />
+         )}
+
+
+         {filteredPosts.length === 0 && (
+            <p>Ops! Não existe posts com esse nome.</p>
+         )}
+        
+        <div className="button-container">
+        {!searchValue && (
+           <Button 
+           text="Load more posts"
+           onClick={this.loadMorePosts}
+           disabled={noMorePosts}
+           />
+        )}
+        </div>
+       </section>
+     );
+  }
 }
 
  
